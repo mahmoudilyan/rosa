7559e2aa46950cb7c739bc65bf38a64d5b3f9179
Mahmoud Ilyan <mahmoudib@live.com>
2015-03-21 20:29:43 +0200

    Search Implemented

---
+++ wp-content/themes/rosa/search.php
@@ -0,0 +1,54 @@
+<?php 
+get_header() ;?>
+
+<div id="content">
+    <div class="row">
+        
+        <div class="col-md-9">
+            
+           
+
+            <?php if(have_posts()) : ?>
+             <h3><?php _e('Search results: ', 'rosa') ;?></h3>
+            <ul class="lists">
+                <?php while(have_posts()) : the_post() ;?>
+                <li>
+                    <div class="img-post">
+                        <?php if( has_post_thumbnail() ){
+                                the_post_thumbnail('single',array('class' => 'img-single-staff'));
+                            }
+                        ?>                        
+                    </div>
+                    <h6><?php the_title() ;?></h6>
+                    <?php the_excerpt() ;?>
+                    <a href="<?php the_permalink() ;?>"><?php _e('Read More ','fespal') ;?></a>
+                </p>
+                <hr>
+
+                <?php endwhile ;?>
+                <?php global $wp_query; ?>
+                </li>
+
+            </ul>
+            <?php fespal_content_nav("pagination", $wp_query); ?>
+            <?php else :?> 
+             <h3><?php _e('No results found', 'rosa') ;?></h3>
+            
+        <?php endif; ?>
+        
+
+        </div>
+
+        <div class="col-md-3">
+            <div class="sidebar">
+                <?php get_sidebar('fow') ;?>                
+                
+
+            </div>
+        </div>
+
+    </div>
+
+
+</div>    
+<?php get_footer() ;?>
